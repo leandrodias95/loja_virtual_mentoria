f812da5bf90b36344b1e83d7aa6265caa8e9ed16
leandrodias95 <leandrogit95@gmail.com>
2024-11-14 00:22:37 -0300

    implementação classes: pessoa e suas subclasses part1

---
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,70 @@
+package jdev.mentoria.lojavirtual.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "pessoa_juridica")
+public class PessoaJuridica extends Pessoa{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String cnpj;
+	private String inscEstadual;
+	private String inscMunicipal;
+	private String nomeFantasia;
+	private String razaoSocial; 
+	private String categoria;
+	@OneToMany(mappedBy = "pessoaJuridica", orphanRemoval = true, cascade = CascadeType.ALL, fetch =FetchType.LAZY) //orphanRemoval = se apaga uma pessoa apaga os endereços
+	private List<Endereco> enderecos =  new ArrayList<Endereco>(); 
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+	public String getInscMunicipal() {
+		return inscMunicipal;
+	}
+	public void setInscMunicipal(String inscMunicipal) {
+		this.inscMunicipal = inscMunicipal;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+	public void setEnderecos(List<Endereco> enderecos) {
+		this.enderecos = enderecos;
+	} 
+
+}
